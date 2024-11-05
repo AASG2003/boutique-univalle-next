2d2ed9644538f2b210cbf4ae01545795c053ab5e
Andrei <betoxd2003@gmail.com>
2024-11-05 08:53:14 -0400

    header para evitar el warning de ngrok

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -4,6 +4,13 @@ import axios from 'axios';
 // Define la URL base de tu API
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL+"/tienda/product";
 
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL+"/tienda/product",
+    headers: {
+      'ngrok-skip-browser-warning': 'true', // Agregar este encabezado para evitar el warning
+      'User-Agent': 'Custom-User-Agent'     // Opción adicional para personalizar el User-Agent
+    }
+  });
 // Define la interfaz para un producto
 export interface Product {
     idproducts: number;
@@ -21,33 +28,32 @@ export interface ProductWithImage {
 
 const productService = {
     async createProduct(data: FormData): Promise<Product> {
-        console.log(API_BASE_URL)
-        const response = await axios.post(`${API_BASE_URL}/create`, data);
+        const response = await api.post(`${API_BASE_URL}/create`, data);
         return response.data;
     },
 
     async fetchAllProducts(): Promise<Product[]> {
-        const response = await axios.get(`${API_BASE_URL}/findAll`);
+        const response = await api.get(`${API_BASE_URL}/findAll`);
         return response.data;
     },
 
     async fetchProductById(id: number): Promise<Product> {
-        const response = await axios.get(`${API_BASE_URL}/findOne/${id}`);
+        const response = await api.get(`${API_BASE_URL}/findOne/${id}`);
         return response.data;
     },
 
     async fetchProductByIdWithImage(id: number): Promise<ProductWithImage> {
-        const response = await axios.get(`${API_BASE_URL}/fetchByIdWithImage/${id}`);
+        const response = await api.get(`${API_BASE_URL}/fetchByIdWithImage/${id}`);
         return response.data;
     },
 
     async updateProduct(id: number, data: Partial<Product>): Promise<Product> {
-        const response = await axios.patch(`${API_BASE_URL}/updateOne/${id}`, data);
+        const response = await api.patch(`${API_BASE_URL}/updateOne/${id}`, data);
         return response.data;
     },
 
     async deleteProduct(id: number): Promise<Product> {
-        const response = await axios.delete(`${API_BASE_URL}/deleteOne/${id}`);
+        const response = await api.delete(`${API_BASE_URL}/deleteOne/${id}`);
         return response.data;
     }
 };
